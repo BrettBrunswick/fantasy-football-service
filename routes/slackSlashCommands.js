f54738f0990c97164f8b237f8e6bf1a97f494298
Brett <brett.brunswick@upstart.com>
2019-08-13 16:45:20 -0400

    cleaned up parsing of record slash command req

--- routes/slackSlashCommands.js
+++ routes/slackSlashCommands.js
@@ -2,21 +2,30 @@ const express = require('express');
 const router = express.Router();
 const SeasonsService = require('../services/seasonService');
 
-const betweenCarrotsRegex = /\<([^)]+)\>/;
-const userNameRegEx = /\|(\S+)/;
-const yearRegEx = /(20)\d{2}/gm;
+const mentionedSlackUsersRegex = /\<([^\s]+)\>/g;
+const userNameRegEx = /\|(\S+)\>/;
+const yearRegEx = /(20)\d{2}/g;
 
 
 router.post('/record', async (req, res) => {
     console.log(req.body)
     const reqUserName = req.body.user_name
     const allSeasons = await SeasonsService.getAllSeasons();
-    let betweenCarrots = req.body.text.match(betweenCarrotsRegex)[1];
-    console.log(`betweenCarrots: ${betweenCarrots}`)
-    var userName = betweenCarrots.match(userNameRegEx)[1];
-    console.log(`User: ${userName}`)
-    var year = req.body.text.match(yearRegEx)
-    console.log(`year: ${year}`)
+    let mentionedSlackUsers = req.body.text.match(mentionedSlackUsersRegex);
+    const mentionedSlackUserNames = [];
+    if (mentionedSlackUsers != null) {
+        console.log(`mentionedSlackUsers count: ${mentionedSlackUsers.length}`)
+        mentionedSlackUsers.forEach(mentionedUser => {
+            console.log(`slack user: ${mentionedUser}`)
+            mentionedUserName = mentionedUser.match(userNameRegEx);
+            mentionedSlackUserNames.push(mentionedUserName[1]);
+            console.log(`slack user name: ${mentionedUserName[1]}`)
+        });
+        var year = req.body.text.match(yearRegEx)
+        console.log(`year: ${year}`)
+    } else {
+        res.status(200).send();
+    }
 })
 
 module.exports = router;
